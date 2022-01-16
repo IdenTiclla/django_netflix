fc4af5f0b8f9052efa9add5889b897b299c1f3b0
IdenTiclla <iden.ticlla@gmail.com>
2022-01-16 13:40:29 -0400

    Login

--- django_netflix/urls.py
+++ django_netflix/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
