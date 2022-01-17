2fc361e35d3e3434bc8decd5d154ca7e787e2ed3
IdenTiclla <iden.ticlla@gmail.com>
2022-01-17 14:57:45 -0400

    Profile List and login required

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 class Home(View):
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('profile_list')
         return render(request, 'index.html')
+
+@method_decorator(login_required, name='dispatch')
+class ProfileList(View):
+    def get(self, request):
+        profiles = request.user.profiles.all()
+        return render(request, 'profileList.html', {'profiles': profiles})
