ee40ce951deaf14494593695baf88e8f2688a9d7
IdenTiclla <iden.ticlla@gmail.com>
2022-01-17 18:17:12 -0400

    profile create completed

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, ProfileList
+from .views import Home, ProfileList, ProfileCreate
 
 urlpatterns = [
     path('', Home.as_view(), name='index'),
     path('profile/', ProfileList.as_view(), name='profile_list'),
+    path('profile/create/', ProfileCreate.as_view(), name='profile_create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
+import profile
 from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
+from core.models import Profile
+from .forms import ProfileForm
+
 # Create your views here.
 
 class Home(View):
@@ -15,3 +20,27 @@ class ProfileList(View):
     def get(self, request):
         profiles = request.user.profiles.all()
         return render(request, 'profileList.html', {'profiles': profiles})
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        form = ProfileForm()
+
+        return render(request, 'profileCreate.html', {
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('profile_list')
+
+        return render(request, 'profileCreate.html', {
+            'form': form
+        })
